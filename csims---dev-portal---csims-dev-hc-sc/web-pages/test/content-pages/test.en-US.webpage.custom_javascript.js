b5fa8e8afb931cfbf5f1db7924f6a4a093a4630b
Frederick Pearson <frederick.pearson@international.gc.ca>
2024-02-09 00:52:01 -0500

    baseline

--- csims---dev-portal---csims-dev-hc-sc/web-pages/test/content-pages/test.en-US.webpage.custom_javascript.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",function() {
-    var selectElement = document.getElementById("subscriptionMultiselect");
-    var showSelectedButton = document.getElementById("showSelected"); 
-    
-    showSelectedButton.addEventListener("click",function() {
-      var selectedOptions = Array.from(selectElement.selectedOptions).map(option => option.value); 
-      console.log("Selected subscriptions: ", selectedOptions);
-// You can perform further actions with the selected options here
-      }); });
